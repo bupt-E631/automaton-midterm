584aa55e820205288ec704c34eba827f4d97cb21
zhuxiao <1317794623@qq.com>
2021-12-06 01:53:26 +0800

    + 添加了e-NFA和NFA的类内变量 + 更新了readme Signed-off-by: zhuxiao <1317794623@qq.com>

--- epsilon_NFA.h
+++ epsilon_NFA.h
@@ -1,8 +1,30 @@
 /*
  * @Author: your name
  * @Date: 2021-12-05 12:19:57
- * @LastEditTime: 2021-12-05 12:19:58
+ * @LastEditTime: 2021-12-06 00:48:01
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \automaton-midterm\epsilon_NFA.h
  */
+#include <iostream>
+#include <vector>
+#include "NFA.h"
+#define MAX_LENGTH 200
+using namespace std;
+class epsilon_NFA
+{
+public:
+    //状态集合，转移函数，开始状态，结束状态集合
+    //所有状态用数字命名
+
+    //Variable:
+    int numOfStatus;
+    int beginStatus;
+    vector<int> Status;
+    vector<vector<int>> transFunc;
+    vector<int> terminalStatus;
+
+    //Function:
+    epsilon_NFA();
+    NFA *toNFA();
+};
