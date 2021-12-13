62b770e28a1a35d4c6e479c012d6a359fe39379b
KarigenB <1837633996@qq.com>
2021-12-13 15:36:57 +0800

    NFA -> DFA 2.0
    Signed-off-by: KarigenB <1837633996@qq.com>

--- NFA.h
+++ NFA.h
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-05 12:20:54
- * @LastEditTime: 2021-12-12 21:37:25
+ * @LastEditTime: 2021-12-13 15:08:13
  * @LastEditors: Karigen B
  * @Description: 完成NFA到DFA的转换
  * @FilePath: \automaton-midterm\NFA.h
@@ -13,9 +13,22 @@
 #include <string>
 #include<set>
 #include<map>
+#include<algorithm>
 #include "FANode.h"
 #include "DFA.h"
 using namespace std;
+
+class Node{
+public:
+    int input;
+    string output;
+    Node() = default;
+    Node(int input, string output) {
+        this->input = input;
+        this->output = output;
+    }
+};
+
 class NFA
 {
 public:
@@ -26,19 +39,158 @@ public:
     int numOfStatus; // 状态数量
     int beginStatus; // 起始状态
     vector<int> Status; // 状态
+    vector<set<int>> myStatus;
     vector<vector<FANode>> transFunc; // 表
     vector<vector<FANode>> transFuncBak;
+    vector<vector<set<int>>> myTransFunc;
     vector<int> terminalStatus; // 结束状态
     vector<int> terminalStatusBak; // 结束状态
-    NFA() {};
+    NFA() {
+        
+    };
     //Function:
-    // 
+    
+    NFA* toDFA(bool isThis){
+        transFuncConvert();//垃圾架构转换为我的架构
+        statusMapping();//垃圾架构状态向量转换为我的向量
+        //根据我的表去做转换
+        for (int i = 0; i < myTransFunc.size(); i++) {
+            for (int j = 0; j < myTransFunc[i].size(); j++) {
+                formExtend(myTransFunc[i][j]);
+            }
+        }
+        
+        //将我的状态映射将垃圾架构的映射改了
+        //根据我的映射状态在去转换为垃圾表
+    }
+
+    void formExtend(set<int> unit){
+        //先判断这个状态是否为空
+        if(unit.size() == 0){
+            return;
+        }
+
+        //判断该状态是否包含于状态表
+        if(exist(unit)){
+            return;
+        }
+
+        // 该集合不包含于状态表,是新集合
+        //加入状态表
+        myStatus.push_back(unit);
+        //表中创建新列 --最后一行存放新状态
+        vector<set<int>> temp;
+        temp.push_back(set<int>());
+        temp.push_back(set<int>());
+        myTransFunc.push_back(temp);
+        //确定输出状态 --新状态实在原生状态的基础上合并得来的,所以各个元素肯定是原原生状态
+        set<int> buffer0;
+        set<int> buffer1;
+        for (set<int>::iterator iter = unit.begin(); iter != unit.end(); iter++) {
+            //输入0
+            set<int> temp0;
+            //myTransFunc[*iter][0]; --为对应集合
+            set_union(myTransFunc[*iter][0].begin(), myTransFunc[*iter][0].end(), buffer0.begin(), buffer0.end(), inserter(temp0, temp0.begin()));
+            buffer0.clear();
+            buffer0 = temp0;
+            //输入1
+            set<int> temp1;
+            //myTransFunc[*iter][1];
+            set_union(myTransFunc[*iter][1].begin(), myTransFunc[*iter][1].end(), buffer1.begin(), buffer1.end(), inserter(temp1, temp1.begin()));
+            buffer1.clear();
+            buffer1 = temp1;
+        }
+        //放入对应行
+        myTransFunc[myTransFunc.size()-1][0] = buffer0;
+        myTransFunc[myTransFunc.size()-1][1] = buffer1;
+    }
+
+    void myDisplay(){
+        for (int i = 0; i < myStatus.size(); i++) {
+            //输表头
+            for (set<int>::iterator iter = myStatus[i].begin(); iter != myStatus[i].end(); iter++) {
+                cout << *iter;
+            }
+            cout << ":";
+            for (set<int>::iterator  iter = myTransFunc[i][0].begin(); iter !=  myTransFunc[i][0].end(); iter++) {
+                cout << *iter;
+            }
+            cout << "  ";
+            for (set<int>::iterator  iter = myTransFunc[i][1].begin(); iter !=  myTransFunc[i][1].end(); iter++) {
+                cout << *iter;
+            }
+            cout << endl;
+        }
+        
+    }
+
+    bool exist(set<int> unit){
+        for (int i = 0; i < myStatus.size(); i++) {
+            if(unit.size() != myStatus[i].size()){//大小不同肯定不是一个状态
+                continue;
+            } else { // 集合个数相等的情况下判断是否是同一个集合
+                //根据对称差集是否为空判断集合是否相等
+                set<int> temp;
+                set_symmetric_difference(unit.begin(), unit.end(), myStatus[i].begin(), myStatus[i].end(), inserter(temp, temp.begin()));
+                if(temp.size() == 0){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    void statusMapping(){
+        for (int i = 0; i < Status.size(); i++) {
+            set<int> temp;
+            temp.insert(Status[i]);
+            myStatus.push_back(temp);
+        }
+    }
+
+    void transFuncConvert(){
+        //1.先构建空表
+        vector<set<int>> temp;
+        temp.push_back(set<int>());
+        temp.push_back(set<int>());
+        for (int i = 0; i < numOfStatus; i++) {
+            myTransFunc.push_back(temp);
+        }
+        //2.将数据放入空表
+        for (int i = 0; i < transFunc.size(); i++) {
+            for (int j = 0; j < transFunc[i].size(); j++) {
+                if(transFunc[i][j].input == 0){
+                    myTransFunc[i][0].insert(transFunc[i][j].output);//将输入1转换过去的状态放入对应位置
+                } else if(transFunc[i][j].input == 1){
+                    myTransFunc[i][1].insert(transFunc[i][j].output);//将输入1转换过去的状态放入对应位置
+                }
+            }
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     DFA* toDFA() {
         transFuncBak = transFunc;
         terminalStatusBak = terminalStatus;
-        //transFunc.clear();
+        //
+        //for (int i = 0; i < transFunc.size(); i++) {
+        //    myTransFunc.push_back(vector<Node>());
+        //}
+        
+
         FANodeConvert();
+
+        // 测试代码
+        for (int i = 0; i < myTransFunc.size(); i++) {
+            for (int j = 0; j < myTransFunc[i].size(); j++) {
+                //cout << myTransFunc[i][j].input << "->" << myTransFunc[i][j].output << endl;
+            }
+            cout << endl;
+        }
+        
+
         // NFA -> DFA
         for (int i = 0; i < transFunc.size(); i++) { // 外层循环,状态数
             for (int j = 0; j < transFunc[i].size(); j++) { // 内层循环,0/1的输入状态方程
@@ -129,14 +281,18 @@ public:
 
             int in0 = 0;
             int in1 = 0;
+            string s0 = "";
+            string s1 = "";
             for (int j = transFunc[i].size() - 1; j >= 0; j--) { // 整数最高位不能是0
                 if (transFunc[i][j].input == 0) { // 输入是0的情况
                     in0 *= 10;
                     in0 += transFunc[i][j].output;
+                    s0 = s0 + to_string(transFunc[i][j].output);
                 }
                 else if (transFunc[i][j].input == 1) { // 输入是1的情况
                     in1 *= 10;
                     in1 += transFunc[i][j].output;
+                    s1 = s1 + to_string(transFunc[i][j].output);
                 }
             }
 
@@ -144,9 +300,15 @@ public:
             if (f0) {
                 transFunc[i].push_back(FANode(0, in0));
             }
+            if (f0) {
+                //myTransFunc[i].push_back(Node(0, s0));
+            }
 
             if (f1) {
                 transFunc[i].push_back(FANode(1, in1));
+            }
+            if (f1) {
+                //myTransFunc[i].push_back(Node(1, s1));
             }
         }
     }
--- epsilon_NFA.h
+++ epsilon_NFA.h
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-05 12:19:57
- * @LastEditTime: 2021-12-12 15:48:53
+ * @LastEditTime: 2021-12-13 00:10:49
  * @LastEditors: Karigen B
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \automaton-midterm\epsilon_NFA.h
